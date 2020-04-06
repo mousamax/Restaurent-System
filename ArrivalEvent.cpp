79f9a558ef546475f66b5a77c20eb2cfdc055886
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2020-04-06 22:45:28 +0200

    Add files via upload
    Events

--- ArrivalEvent.cpp
+++ ArrivalEvent.cpp
@@ -1,21 +1,27 @@
 #include "ArrivalEvent.h"
 #include "..\Rest\Restaurant.h"
 
-
-ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType):Event(eTime, oID)
+ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType, int SIZE, double Money) :Event(eTime, oID)
 {
 	OrdType = oType;
+	Size = SIZE;
+	OrdMoney = Money;
+	TimeStep = eTime;
+
 }
 
 void ArrivalEvent::Execute(Restaurant* pRest)
 {
 	//This function should create an order and fills its data 
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
+	Order* pOrd = new Order(OrderID, OrdType);
+	pOrd->setarrivaltime(TimeStep);
+	pOrd->setordersize(Size);
+	pOrd->setTotalMoney(OrdMoney);
 
-	
-	
+	pRest->AddtoQueue(pOrd);
 	///For the sake of demo, this function will just create an order and add it to DemoQueue
 	///Remove the next code lines in phases 1&2
-	Order* pOrd = new Order(OrderID,OrdType);
+
 	pRest->AddtoDemoQueue(pOrd);
 }
